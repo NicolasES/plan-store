0aa9055b4b9bc00162c847c24dd3feba0093248c
NicolasES <nicolas.escouto@gmail.com>
2022-10-20 16:20:24 -0300

    MemoryShoppingCartRepository

--- backend/src/domain/repositories/ShoppingCartRepository.ts
+++ backend/src/domain/repositories/ShoppingCartRepository.ts
@@ -2,5 +2,5 @@ import ShoppingCart from '@entities/ShoppingCart'
 
 export default interface ShoppingCartRepository {
   getShoppingCart(): Promise<ShoppingCart>
-  save(shoppingCart: ShoppingCart): Promise<ShoppingCart>
+  save(shoppingCart: ShoppingCart): Promise<void>
 }
